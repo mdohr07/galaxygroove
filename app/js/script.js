c1d19c28738cd3f3239850bec3c2facadcd1a1d1
m-dohr-07 <mdohr07@gmail.com>
2023-06-23 17:09:41 +0200

    JavaScript

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,13 @@
-console.log('hallo Welt!');
+const burgerBtn = document.querySelector('#burgerBtn');
+
+burgerBtn.addEventListener('click', function() {
+    console.log('click hamburger');
+
+    if (burgerBtn.classList.contains('open')) {
+        burgerBtn.classList.remove('open');
+    }
+    else {
+    burgerBtn.classList.add('open'); 
+    }
+    
+})
