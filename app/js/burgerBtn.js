a40e931a3911eab82effacd4a7dc35d9529b7be8
m-dohr-07 <mdohr07@gmail.com>
2023-06-23 17:22:55 +0200

    updates to JavaScript

---
+++ app/js/burgerBtn.js
@@ -0,0 +1,12 @@
+const burgerBtn = document.querySelector('#burgerBtn');
+burgerBtn.addEventListener('click', function () {
+    console.log('click hamburger');
+
+    if (burgerBtn.classList.contains('open')) {
+        burgerBtn.classList.remove('open');
+    }
+    else {
+        burgerBtn.classList.add('open');
+    }
+
+});
