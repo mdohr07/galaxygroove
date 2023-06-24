76b29160faf3c6f26f6c4e305710fe4c092bcd59
m-dohr-07 <mdohr07@gmail.com>
2023-06-24 15:56:54 +0200

    updates to JS, globals & header

--- app/js/script.js
+++ app/js/script.js
@@ -1,13 +1,18 @@
-const burgerBtn = document.querySelector('#burgerBtn');
+const burgerBtn = document.querySelector('#burgerBtn'); 
+// Weist der Konstante burgerBtn das Element mit der ID #burgerBtn zu
+/* document.querySelector('burgerBtn') findet das Element mit der 
+   angegebenen ID und weist es der Konstante zu */
 
 burgerBtn.addEventListener('click', function() {
-    console.log('click hamburger');
+// Fügt Event-Listener zu burgerBtn hinzu
+//Event-Listener wartet auf Click-Event
 
-    if (burgerBtn.classList.contains('open')) {
-        burgerBtn.classList.remove('open');
+
+    if (burgerBtn.classList.contains('open')) { // Prüft auf Klasse "Open"
+        burgerBtn.classList.remove('open'); //Falls enthalten: entferne "open"
     }
     else {
-    burgerBtn.classList.add('open'); 
+    burgerBtn.classList.add('open'); // Falls nicht: füge "open" hinzu
     }
     
 });
